8f8db445c85982d89557570cdeff77e7a75fd043
swawiye <sovereign.wawiye@gmail.com>
2025-06-18 08:09:08 +0300

    footer

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {'message' : 'Home Page'}
-    return render(request, 'index.html', context)
+    #context = {'message' : 'Home Page'}
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
